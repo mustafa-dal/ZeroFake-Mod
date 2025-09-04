8e889ffda4a2182aa1238f7a9ad2a7f1818e9087
Mustafa Dal <1mustafa.dal@gmail.com>
2025-09-05 00:19:53 +0300

    solving dep

--- blipmodels/med.py
+++ blipmodels/med.py
@@ -37,12 +37,12 @@ from transformers.modeling_outputs import (
     TokenClassifierOutput,
 )
 from transformers.modeling_utils import (
-    PreTrainedModel,
+    apply_chunking_to_forward, 
     find_pruneable_heads_and_indices,
+    PreTrainedModel,
     prune_linear_layer,
 )
 
-from transformers import apply_chunking_to_forward
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 from transformers.generation.utils import GenerationMixin
