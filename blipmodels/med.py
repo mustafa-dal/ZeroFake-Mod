7eb66be4ccc8760cedf696194ba19d2a6f3b1025
Mustafa Dal <1mustafa.dal@gmail.com>
2025-09-05 11:47:01 +0300

    pretrained correction

--- blipmodels/med.py
+++ blipmodels/med.py
@@ -39,10 +39,9 @@ from transformers.modeling_outputs import (
 from transformers.pytorch_utils import (
     apply_chunking_to_forward, 
     find_pruneable_heads_and_indices,
-    PreTrainedModel,
     prune_linear_layer,
 )
-
+from transformers import PreTrainedModel
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 from transformers.generation.utils import GenerationMixin
